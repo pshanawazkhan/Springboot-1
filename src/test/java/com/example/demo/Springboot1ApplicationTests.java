91c0b90d2edfcf136594525d0cca86616d442fa2
pshanawazkhan <pshanawazkhan@gmail.com>
2024-08-03 11:33:00 +0530

    test case 1

--- src/test/java/com/example/demo/Springboot1ApplicationTests.java
+++ src/test/java/com/example/demo/Springboot1ApplicationTests.java
@@ -1,5 +1,7 @@
 package com.example.demo;
 
+import static org.junit.jupiter.api.Assertions.assertEquals;
+
 import org.junit.jupiter.api.Test;
 import org.springframework.boot.test.context.SpringBootTest;
 
@@ -8,6 +10,21 @@ class Springboot1ApplicationTests {
 
 	@Test
 	void contextLoads() {
+		
+		
+		 // Arrange
+        String str1 = "Hello";
+        String str2 = "World";
+
+        // Act
+        String result = str1 + " " + str2;
+
+        // Assert
+        String expected = "Hello World";
+        assertEquals(expected, result);
+		
+		
+		
 	}
 
 }
